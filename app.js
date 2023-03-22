4e136b5136c73126e318ca99b01d537ca9a73246
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-03-22 16:48:34 +0100

    made functions and grid for dialogwindow

--- app.js
+++ app.js
@@ -2,28 +2,69 @@
 
 // ============ LOAD & INIT APP ============ //
 window.addEventListener("load", initApp);
+const harry = {
+  name: "Harry Potter",
+  species: "human",
+  gender: "male",
+  house: "Gryffindor",
+  dateOfBirth: "31-07-1980",
+  yearOfBirth: 1980,
+  ancestry: "half-blood",
+  eyeColour: "green",
+  hairColour: "black",
+  wand: "holly,phoenix feather,11",
+  patronus: "stag",
+  hogwartsStudent: true,
+  hogwartsStaff: false,
+  actor: "Daniel Radcliffe",
+  alive: true,
+  image: "http://hp-api.herokuapp.com/images/harry.jpg",
+};
+
+// Hermione Granger
+const hermione = {
+  name: "Hermione Granger",
+  species: "human",
+  gender: "female",
+  house: "Gryffindor",
+  dateOfBirth: "19-09-1979",
+  yearOfBirth: 1979,
+  ancestry: "muggleborn",
+  eyeColour: "brown",
+  hairColour: "brown",
+  wand: "vine,dragon heartstring",
+  patronus: "otter",
+  hogwartsStudent: true,
+  hogwartsStaff: false,
+  actor: "Emma Watson",
+  alive: true,
+  image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+};
+
+// Ron Weasley
+const ron = {
+  name: "Ron Weasley",
+  species: "human",
+  gender: "male",
+  house: "Gryffindor",
+  dateOfBirth: "01-03-1980",
+  yearOfBirth: 1980,
+  ancestry: "pure-blood",
+  eyeColour: "blue",
+  hairColour: "red",
+  wand: "willow,unicorn tail-hair,14",
+  patronus: "Jack Russell terrier",
+  hogwartsStudent: true,
+  hogwartsStaff: false,
+  actor: "Rupert Grint",
+  alive: true,
+  image: "http://hp-api.herokuapp.com/images/ron.jpg",
+};
 
 function initApp() {
-    // Harry Potter
-    const potterName = "Harry Potter";
-    const potterGender = "male";
-    const potterHouse = "Gryffindor";
-    const potterDateOfBirth = "31-07-1980";
-    const potterAncestry = "half-blood";
-    const potterEyeColour = "green";
-    const potterHairColour = "black";
-    const potterActor = "Daniel Radcliffe";
-    const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
+  showCharacter(harry);
+}
 
-    console.log(
-        potterName,
-        potterGender,
-        potterHouse,
-        potterDateOfBirth,
-        potterAncestry,
-        potterEyeColour,
-        potterHairColour,
-        potterActor,
-        potterImage
-    );
+function showCharacter(character) {
+  console.log(harry);
 }
